8a538e78efc541ea7d30bb4e4596bb86659fcd2c
Silaenn <deokeldisilaen@gmail.com>
2025-06-23 17:42:34 +0700

    update layout

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="relative min-h-screen flex flex-col items-center justify-center bg-[url('/images/BackgroundLogin.png')] bg-cover bg-center bg-no-repeat py-12 sm:px-6 lg:px-8 px-6">
+    <div className="text-black relative min-h-screen flex flex-col items-center justify-center bg-[url('/images/BackgroundLogin.png')] bg-cover bg-center bg-no-repeat py-12 sm:px-6 lg:px-8 px-6">
       <div className="absolute inset-0 bg-black opacity-10 z-0"></div>
       <div className="relative z-10 w-full">
         <AuthForm />
